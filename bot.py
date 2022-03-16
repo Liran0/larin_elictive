d8b2078c81077fc8ca1e2c3613656f072c7c934e
Student <t_larin05>
2022-03-16 14:20:17 +0300

    мношо сделали

--- bot.py
+++ bot.py
@@ -3,16 +3,42 @@ from key import TOKEN
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from openpyxl import load_workbook
 
+
+def new_sticker(updade: Update, context: CallbackContext):
+    sticker_id = updade.message.sticker.file_id
+    for keyword in stickers:
+        if sticker_id == stickers[keyword]:
+            update.message.reply_text(' у меня тоже такой есть ')
+            updade.message.reply_sticker(sticker_id)
+            break
+    else:
+        context.user_data['new_sticker'] = sticker_id
+        updade.message.reply_text('скажи мне ключевое слово для стикера')
+
+
+def new_keyword(updade: Update, context: CallbackContext):
+    if new_sticker not in context.user_data:
+        sey_smth(updade, context)
+    else:
+        keyword = update.massage.text
+        sticker_id = context.user_data['new sticker']
+        insert_sticker(keyword, sticker_id)
+        context.user_data.clear()
+
+
 def insert_sticker(keyword, sticker_id=None, reply_text=None):
     row = stickers_page.max_row +1
     sticker_page.cell(row=row, coum=1).value = keyword
     sticker_page.cell(row=row, coum=1).value = sticker_id
     sticker_page.cell(row=row, coum=1).value = reply_text
     bd.save('base.xlsx')
+
     stickers[keyword] = sticker_id
     replies[keyword] = reply_text
 
+
 bd = load_workbook('base.xlsx')
+
 
 #for row in range(2, stickers_page.max_row +1):
 
@@ -27,6 +53,7 @@ def main():
     keybord_handler = MessageHandler(Filters.text('клавиатура'), keybord)
     exo_handler = MessageHandler(Filters.all, exo)
     hallo_handler = MessageHandler(Filters.text('Привет, привет'), say_hallo)
+    sticker_hendler = MessageHandler(Filters.sticker,  new_sticker)
 
     dispatcher.add_handler(hallo_handler)
     dispatcher.add_handler(keybord_handler)
@@ -34,6 +61,7 @@ def main():
     dispatcher.add_handler(text_handler)
 
     updater.start_polling()
+    print('Бот успешно запустился')
     updater.idle()
 
 
@@ -41,9 +69,6 @@ def say_hallo(update, context):
     name = update.message.from_user.first_name
     text = update.message.text
     update.message.reply_text(text="привет")
-
-
-
 
 
 def exo(update: Update, context: CallbackContext):
@@ -69,6 +94,18 @@ def keybord(update: Update, context: CallbackContext):
 
         )
     )
+def say_smth(update: Update, context: CallbackContext):
+    name = update.message.from_user.first_name
+    text = update.message.text
+    for keyword in stickers:
+        if keyword in text:
+            if stickers[keyword]:
+                update.message.reply_sticker(stickers[keyword])
+            if replies[keyword]:
+                update.message.reply_text(replies[keyword].format(name))
+            break
+    else:
+        exo(update, context)
 
 
 def sey_no(update: Update, context: CallbackContext):
