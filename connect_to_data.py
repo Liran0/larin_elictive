2ca78ed094fbb8e1948e9152fa988649aeb1580a
Liran0 <98091398+Liran0@users.noreply.github.com>
2022-05-10 15:27:59 +0300

    add

---
+++ connect_to_data.py
@@ -0,0 +1,55 @@
+from openpyxl import load_workbook
+
+
+def insert_sticker(keyword, sticker_id=None, reply_text=None):
+    row = stickers_page.max_row + 1
+    stickers_page.cell(row=row, column=1).value = keyword
+    stickers_page.cell(row=row, column=2).value = sticker_id
+    stickers_page.cell(row=row, column=3).value = reply_text
+    bd.save('data_base.xlsx')
+
+    stickers[keyword] = sticker_id
+    replies[keyword] = reply_text
+
+
+def in_database(user: int) -> bool:
+    '''
+    возвращает True, если id пользователь есть в database
+    '''
+    for row in range(1, user_page.max_row + 1):
+        if user == user_page.cell(row=row, column=1).value:
+            return True
+        return False
+
+
+def insert_user(*args):#принимает произвольное количество аргументов
+    '''
+    вносит нового пользователя в базу данных
+    '''
+    row = user_page.max_row + 1
+    user_page.cell(row=row, column=1).value = args[0]
+    user_page.cell(row=row, column=2).value = args[1]
+    user_page.cell(row=row, column=3).value = args[2]
+    user_page.cell(row=row, column=4).value = args[3]
+    bd.save('data_base.xlsx')
+
+
+bd = load_workbook('data_base.xlsx')
+for sheet in bd:
+    print(sheet.title)
+stickers_page = bd['stickers']
+user_page = bd['users']
+
+stickers = {}
+replies = {}
+
+for row in range(1, stickers_page.max_row + 1):
+    keyword = stickers_page.cell(row=row, column=1).value
+    sticker_id = stickers_page.cell(row=row, column=2).value
+    reply_text = stickers_page.cell(row=row, column=3).value
+    stickers[keyword] = sticker_id
+    replies[keyword] = reply_text
+
+
+if __name__ == '__main__':
+    insert_user(12345, 'Liran', 'ж', '10 o')
